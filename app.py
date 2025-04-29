e18290bf21c7919a4c5a91cf6261890219341ac8
AJRishab <ajrstudentid@gmail.com>
2025-04-29 23:10:30 +0530

    Update: fixed deprecated use_column_width to use_container_width

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ uploaded_file = st.file_uploader("Choose a skin image...", type=["jpg", "jpeg", 
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.image(image, caption='Uploaded Image', use_container_width=True)
 
     img = image.resize((IMAGE_SIZE, IMAGE_SIZE))
     img_array = np.array(img) / 255.0
